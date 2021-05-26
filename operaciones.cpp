2a897c5dab03bfca5b38375db537b1392ae65cc1
jordan quishpi <jiquishp1@espe.edu.ec>
2021-05-25 21:43:02 -0500

    desarollo la fucion valida numero

--- operaciones.cpp
+++ operaciones.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
-main(){
-    int n;
-    do {
-        count<<"igrese un numero : ";
-        cin>>n:
-    }while (n<1 || n>10);
+int validanumero(int n){
+    //proceso de validacion
 
+}
+main(){
 
 }
