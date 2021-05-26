22901e2e30cb918c859169ba5c826bafea3b5a06
jordan quishpi <jiquishp1@espe.edu.ec>
2021-05-25 22:22:06 -0500

    llamando a la funcion saludo desed main

--- operaciones.cpp
+++ operaciones.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 using namespace std;
+int validanumero(){
+    int numero;
+    do{
+        cout<<"Ingrese un numero :  ";
+        cin >>numero;
+    }while( numero<1 || numero >10);
+    return numero;
+
+}
 main(){
     //Nesito crear un proceso para validar datos de ingreso
-    int n;
-    cout<<"Ingrese un numero :  ";
-    cin >>n;
+    int n= validanumero();
+    saludoprogramador("juan");
+
 
 
 }
